25a72b9bcdf75db1dbab97340b288333bb4669d9
suspicious-guy <oz0901964@gmail.com>
2026-03-04 04:41:28 +0700

    v 0.1.6 dev
    Added some functions in interpretator
    
    Co-Authored-By: Crushka <165744005+Crushka@users.noreply.github.com>

--- JS/interpretator.js
+++ JS/interpretator.js
@@ -31,20 +31,23 @@ function main() {
         body: [
             {
                 type: "set",
-                name: "x",
+                name: "arr",
                 value: {
-                    type: "sub",
-                    left: {
-                        type: "division",
-                        left: {type: "number", value: 6},
-                        right: {type: "number", value: 2}
-                    },
-                    right: {type: "number", value: 2}
+                    type: "array",
+                    elements: [
+                        { type: "number", value: 5 },
+                        { type: "number", value: 2 },
+                        { type: "number", value: 8 }
+                    ]
                 }
             },
             {
                 type: "print",
-                value: {type: "variable", name: "x"}
+                value: {
+                    type: "array_get",
+                    array: "arr",
+                    index: { type: "number", value: 1 }
+                }
             }
         ]
     };
@@ -56,9 +59,39 @@ function main() {
 }
 
 function compileProgram(node, instructions) {
-    node.body.forEach(statement => 
-        compileStatement(statement, instructions)
-    );
+        for (let i = 0; i < node.body.length; i++) {
+        let current = node.body[i];
+
+        if (current.type === "if") {
+            compileExpression(current.condition, instructions);
+
+            let jumpIfFalseIndex = instructions.length;
+            instructions.push({ type: "jump_if_false", address: null });
+
+            current.body.forEach(stmt =>
+                compileStatement(stmt, instructions)
+            );
+            let next = node.body[i + 1];
+            if (next && next.type === "else") {
+                let jumpIndex = instructions.length;
+                instructions.push({ type: "jump", address: null });
+                instructions[jumpIfFalseIndex].address = instructions.length;
+
+                next.body.forEach(stmt =>
+                    compileStatement(stmt, instructions)
+                );
+                instructions[jumpIndex].address = instructions.length;
+                i++;
+            } else {
+                instructions[jumpIfFalseIndex].address = instructions.length;
+            }
+
+        } else if (current.type === "else") {
+            siteLog("Ошибка: else без if", true);
+        } else {
+            compileStatement(current, instructions);
+        }
+    }
 }
 
 function compileExpression(node, instructions) {
@@ -68,6 +101,30 @@ function compileExpression(node, instructions) {
             break;
         case "variable":
             instructions.push({type: "load", name: node.name});
+            break;
+        case "array":
+            node.elements.forEach(el =>
+                compileExpression(el, instructions)
+            );
+            instructions.push({type: "array_create", count: node.elements.length});
+            break;
+        case "array_get":
+            instructions.push({ type: "load", name: node.array });
+            compileExpression(node.index, instructions);
+            instructions.push({ type: "array_get" });
+            break;
+        case "array_set":
+            instructions.push({ type: "load", name: node.array });
+            compileExpression(node.index, instructions);
+            compileExpression(node.value, instructions);
+            instructions.push({
+                type: "array_set",
+                name: node.array
+            });
+            break;
+        case "array_length":
+            instructions.push({ type: "load", name: node.array });
+            instructions.push({ type: "array_length" });
             break;
         case "add":
             compileExpression(node.left, instructions);
@@ -88,6 +145,32 @@ function compileExpression(node, instructions) {
             compileExpression(node.left, instructions);
             compileExpression(node.right, instructions);
             instructions.push({type: "division"});
+            break;
+
+        case "less":
+            compileExpression(node.left, instructions);
+            compileExpression(node.right, instructions);
+            instructions.push({type: "less"});
+            break;
+        case "more":
+            compileExpression(node.left, instructions);
+            compileExpression(node.right, instructions);
+            instructions.push({type: "more"});
+            break;
+        case "equal":
+            compileExpression(node.left, instructions);
+            compileExpression(node.right, instructions);
+            instructions.push({type: "equal"});
+            break;
+        case "less or equal":
+            compileExpression(node.left, instructions);
+            compileExpression(node.right, instructions);
+            instructions.push({type: "less or equal"});
+            break;
+        case "more or equal":
+            compileExpression(node.left, instructions);
+            compileExpression(node.right, instructions);
+            instructions.push({type: "more or equal"});
             break;
     }
 }
@@ -114,6 +197,18 @@ function run(instructions) {
         let instr = instructions[ip];
         let b, a;
         switch (instr.type) {
+            case "jump":
+                ip = instr.address;
+                continue;
+
+            case "jump_if_false":
+                let condition = stack.pop();
+                if (!condition) {
+                    ip = instr.address;
+                    continue;
+                }
+                break;
+
             case "push":
                 stack.push(instr.value);
                 break;
@@ -123,6 +218,33 @@ function run(instructions) {
             case "store":
                 variables[instr.name] = stack.pop();
                 break;
+
+            // МАССИВЫ
+            case "array_create":
+                let newArray = [];
+                for (let i = 0; i < instr.count; i++) {
+                    newArray.unshift(stack.pop());
+                }
+                stack.push(newArray);
+                break;
+            case "array_get":
+                let index = stack.pop();
+                let array = stack.pop();
+                stack.push(array[index]);
+                break;
+            case "array_set":
+                let value = stack.pop();
+                let idx = stack.pop();
+                let arr = stack.pop();
+                arr[idx] = value;
+                variables[instr.name] = arr;
+                break;
+            case "array_length":
+                let a = stack.pop();
+                stack.push(a.length);
+                break;
+
+            // АРИФМЕТИЧЕСКИЕ ОПЕРАЦИИ
             case "add":
                 b = stack.pop();
                 a = stack.pop();
@@ -143,6 +265,34 @@ function run(instructions) {
                 a = stack.pop();
                 stack.push(a / b);
                 break;
+
+            // ОПЕРАТОРЫ СРАВНЕНИЯ
+            case "less":
+                b = stack.pop();
+                a = stack.pop();
+                stack.push(a < b);
+                break;
+            case "more":
+                b = stack.pop();
+                a = stack.pop();
+                stack.push(a > b);
+                break;
+            case "equal":
+                b = stack.pop();
+                a = stack.pop();
+                stack.push(a == b);
+                break;
+            case "less or equal":
+                b = stack.pop();
+                a = stack.pop();
+                stack.push(a <= b);
+                break;
+            case "more or equal":
+                b = stack.pop();
+                a = stack.pop();
+                stack.push(a >= b);
+                break;
+                
             case "print":
                 siteLog(stack.pop());
                 break;
